5dc0eb58e136b07e74a513408ab9de5dd1c69f71
Wenting Song <song1992katherine@hotmail.com>
2016-08-26 20:42:59 -0400

    Update frequentChar.java

--- Java/frequentChar.java
+++ Java/frequentChar.java
@@ -1,59 +1,40 @@
 import java.util.HashMap;
 import java.util.Set;
 
-
 /*"Given an input string, return the frequency of the most frequent character."
 Your function should also handle the empty string as an input.
  */
-public class frequency{
-	public static int Max(String words){
-		HashMap<Character,Integer> map = new HashMap<Character,Integer>(); 
-		char[] charwords = words.toCharArray();
-		for(char ch:charwords){
-			
-		    if(map.containsKey(ch)){
-		    	map.put(ch,map.get(ch)+1);
-		    	
-		    }else{
-		    	map.put(ch,1);
-		    }
-		}
-			
-		Set<Character> set = map.keySet();
-        int max = 0;
-	    for (char c : set) {
-	        if (max == 0 || map.get(c) > max) {
-	            max = map.get(c);
+public class frequency {
+	   public static int Max(String words) {
+	        HashMap<Character, Integer> map = new HashMap<Character, Integer>();
+	        int max = 0;
+	        char[] charwords = words.toCharArray();
+	        for (char ch : charwords) {
+	            int frequency;
+	            if (map.containsKey(ch)) {
+	                frequency = map.get(ch) + 1;
+	            } else {
+	                frequency = 1;
+	            }
+	            map.put(ch, frequency);
+
+	            if (frequency > max) {
+	                max = frequency;
+	            }
 	        }
+	        return max;
 	    }
 
-	    for (Character o : map.keySet()) {
-	        if (map.get(o).equals(max)) {
-	            System.out.println(o);
-	        }
-	    }
 	
-		return max;
-	}
+
 
 	public static void main(String[] args) {
-	   
-	   System.out.println(Max("aabbcccduuuu"));
+
+		System.out.println(Max("aabbcccduuuu"));
 	}
-			
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-	}
+
+}
+
 	
 	
 	
