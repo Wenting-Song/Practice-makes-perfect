7aa90cd94421a23e5504a041d813b6d6ab0465c3
Wenting Song <song1992katherine@hotmail.com>
2016-08-26 20:46:53 -0400

    Update frequentChar.java

--- Java/frequentChar.java
+++ Java/frequentChar.java
@@ -1,41 +1,16 @@
-import java.util.HashMap;
-import java.util.Set;
+//Thank you so much for Ben Babtsov's help:
 
-/*"Given an input string, return the frequency of the most frequent character."
-Your function should also handle the empty string as an input.
- */
-public class frequency {
-	   public static int Max(String words) {
-	        HashMap<Character, Integer> map = new HashMap<Character, Integer>();
-	        int max = 0;
-	        char[] charwords = words.toCharArray();
-	        for (char ch : charwords) {
-	            int frequency;
-	            if (map.containsKey(ch)) {
-	                frequency = map.get(ch) + 1;
-	            } else {
-	                frequency = 1;
-	            }
-	            map.put(ch, frequency);
-
-	            if (frequency > max) {
-	                max = frequency;
-	            }
-	        }
-	        return max;
-	    }
-
-	
-
-
-	public static void main(String[] args) {
-
-		System.out.println(Max("aabbcccduuuu"));
-	}
-
-}
-
-	
+    public static int Max(String words) {
+        HashMap<Character, Integer> map = new HashMap<Character, Integer>();
+        int max = 0;
+        char[] charwords = words.toCharArray();
+        for (char ch : charwords) {
+            int frequency = map.containsKey(ch) ? map.get(ch) + 1 : 1;
+            map.put(ch, frequency);
+            max = Math.max(max, frequency);
+        }
+        return max;
+    }	
 	
 	
 	
