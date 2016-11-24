efc6051761a1176f84a21985e5a4d23a3f823970
Wenting Song <song1992katherine@hotmail.com>
2016-11-24 01:43:31 -0500

    Create mergeTwoSortedArrays.java

---
+++ Java/mergeTwoSortedArrays.java
@@ -0,0 +1,50 @@
+//merge two sorted arrays into one combined array such that the combined array will be sorted as well.
+//output is 1 2 2 3 4 4 5 7 8
+//for example: int A[] = {1,2,3,4,5}; int B[] = {2,4,7,8};
+//output is 1 2 2 3 4 4 5 7 8
+
+public class mergeTwo {
+
+public static int[] merge(int[] A, int[] B) {
+	int a = A.length;
+	int b = B.length;
+	int result[] = new int[a + b];
+	int i = 0;
+	int j = 0;
+	int k = 0;
+	while (true) {
+		if (i < a && j < b) {
+			if (A[i] < B[j]) {
+				result[k++] = A[i];
+				i++;
+			} else {
+				result[k++] = B[j];
+				j++;
+			}
+		}
+
+		else if (i >= a) {
+			result[k++] = B[j];
+			j++;
+		} else {
+			result[k++] = A[i];
+			i++;
+
+		}
+		if (i >= a && j >= b)
+			break;
+	}
+
+	return result;
+}
+
+public static void main(String[] args) {
+	int A[] = { 1, 2, 3, 4 };
+	int B[] = { 2, 4, 7, 8 };
+
+	int[] c = merge(A, B);
+	for (int elem : c) {
+		System.out.print(elem + " ");
+	}
+}
+}
