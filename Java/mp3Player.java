2c7e34ece932c0e8ce860284b72cb571a809d4bc
Wenting Song <song1992katherine@hotmail.com>
2016-12-01 12:10:46 -0500

    Update mp3Player.java

--- Java/mp3Player.java
+++ Java/mp3Player.java
@@ -1,15 +1,14 @@
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
 
 /*you have an mp3 player with two buttons, up and down
 and you have a list of songs
 what's the minimum amount of button presses do you need to go?*/
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+
 public class mp3Player {
 	public static int clicksToSong(String[] songs, int startIndex, String destinationSong) {
-		// for(int i =0; i<songs.length;i++){
 		ArrayList<Integer> indexList = new ArrayList<Integer>();
-		// int index = Arrays.asList(songs).indexOf(destinationSong);
 		for (int i = 0; i < songs.length; i++) {
 			if (Arrays.asList(songs).get(i).equals(destinationSong))
 				indexList.add(Math.abs(startIndex - i));
@@ -17,8 +16,6 @@ public class mp3Player {
 		}
 
 		int ret = Collections.min(indexList);
-		// int click = Math.abs(startIndex - index);
-		// int ret = Math.min(click, songs.length - click);
 		return ret;
 
 	}
