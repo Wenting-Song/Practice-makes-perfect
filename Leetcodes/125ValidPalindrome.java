ccad88f57711c83e584f2a9060aebb303cc53fb5
Wenting Song <song1992katherine@hotmail.com>
2016-12-07 12:33:43 -0500

    Update 125ValidPalindrome.java

--- Leetcodes/125ValidPalindrome.java
+++ Leetcodes/125ValidPalindrome.java
@@ -8,17 +8,19 @@ public class Solution {
 	public boolean isPalindrome(String s) {
 		s = s.toLowerCase();
 		s = s.replaceAll("[^a-zA-Z0-9]", "");
-		if(s.length()==0) return true;
-		if(s.length()==1) return true;
+		if (s.length() == 0)
+			return true;
+		if (s.length() == 1)
+			return true;
 		int i = 0;
 		int j = s.length() - 1;
 		while (i <= j) {
-	    	if ((s.charAt(i) == s.charAt(j))) {
+			if ((s.charAt(i) == s.charAt(j))) {
 				i++;
 				j--;
 
-			}
-			else return false;
+			} else
+				return false;
 
 		}
 		return true;
