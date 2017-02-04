44ce2f9fdff2cd084b3254112b4f6ca74568b27f
Wenting Song <song1992katherine@hotmail.com>
2017-02-03 20:20:24 -0500

    Update 268. Missing Number.java

--- Leetcodes/268. Missing Number.java
+++ Leetcodes/268. Missing Number.java
@@ -12,3 +12,14 @@ public class Solution {
 		
 	}
 }
+
+/*Solution2:
+public class Solution {
+	public int missingNumber(int[] nums) {
+		int xor = 0;
+		for (int i = 0; i < nums.length; i++) {
+			xor = xor ^ nums[i] ^ (i + 1);
+		}
+		return xor;
+	}
+}*/
