e3bbaad78778cf8b86a24a8d7f515348960cf0e0
Wenting Song <song1992katherine@hotmail.com>
2017-02-03 20:18:01 -0500

    Update 268. Missing Number.java

--- Leetcodes/268. Missing Number.java
+++ Leetcodes/268. Missing Number.java
@@ -1,16 +1,13 @@
 public class Solution {
 	public int missingNumber(int[] nums) {
-		int len = nums.length;
+		int n = nums.length;
 		int realSum = 0;
 		for (int i = 0; i < nums.length; i++) {
 			realSum += nums[i];
 			
 		}
-		int sum=0;
-		for (int i = 0; i <= len; i++) {
-			sum += i;
-			
-		}
+		int sum=n*(n+1)/2;
+	
 		return sum-realSum;
 		
 	}
