ce2294c2e46f6bcc0ee7af63d6145150289a7940
Wenting Song <song1992katherine@hotmail.com>
2017-02-19 20:45:30 -0500

    Create 520. Detect Capital.java

---
+++ Leetcodes/520. Detect Capital.java
@@ -0,0 +1,23 @@
+/*
+Given a word, you need to judge whether the usage of capitals in it is right or not.
+
+We define the usage of capitals in a word to be right when one of the following cases holds:
+
+All letters in this word are capitals, like "USA".
+All letters in this word are not capitals, like "leetcode".
+Only the first letter in this word is capital if it has more than one letter, like "Google".
+Otherwise, we define that this word doesn't use capitals in a right way.*/
+public class Solution {
+    public boolean detectCapitalUse(String word) {
+    	int upper = 0;
+    	int lower = 0;
+    	for(int i=0;i<word.length();i++){
+        if(Character.isUpperCase(word.charAt(i))) upper++;
+        else lower++;
+    	}
+    	if(upper==word.length()||lower == word.length()||Character.isUpperCase(word.charAt(0))&&upper==1){
+    		return true;
+    	}
+    	return false;
+    }
+}
