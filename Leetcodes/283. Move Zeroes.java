8abab60d905de073b200dcd51c8ecce5c10c5169
Wenting Song <song1992katherine@hotmail.com>
2017-04-01 10:47:19 -0400

    Create 283. Move Zeroes.java

---
+++ Leetcodes/283. Move Zeroes.java
@@ -0,0 +1,29 @@
+/*
+Given an array nums, write a function to move all 0's to the end of it while maintaining the relative order of the non-zero elements.
+
+For example, given nums = [0, 1, 0, 3, 12], after calling your function, nums should be [1, 3, 12, 0, 0].*/
+import java.time.format.FormatStyle;
+import java.util.ArrayList;
+
+public class Solution {
+	public void moveZeroes(int[] nums) {
+
+		int fast = 0;
+		int slow = 0;
+		for (int i = 0; i < nums.length; i++) {
+			if (nums[fast] == 0) {
+				fast++;
+			} else {
+				nums[slow] = nums[fast];
+				fast++;
+				slow++;
+			}
+
+		}
+		while(slow<nums.length){
+			nums[slow++]=0;
+			
+		}
+
+	}
+}
