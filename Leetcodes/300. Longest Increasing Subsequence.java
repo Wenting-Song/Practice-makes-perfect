01c28c145f47fd799acdeaf88f49f1a3f5e7eba6
Wenting Song <song1992katherine@hotmail.com>
2017-05-11 00:03:38 -0400

    Create 300. Longest Increasing Subsequence.java

---
+++ Leetcodes/300. Longest Increasing Subsequence.java
@@ -0,0 +1,22 @@
+public class Solution {
+    public int lengthOfLIS(int[] nums) {
+        	if(nums.length <= 1) 
+			return nums.length;
+    	int[] T = new int[nums.length];
+    	Arrays.fill(T,1);
+    	for(int i=1;i<nums.length;i++){
+    		
+    		for(int j=0;j<i;j++){
+    			if(nums[j]<nums[i]){
+    				if(T[j]+1>T[i]){
+    					T[i] = T[j]+1;
+    				}
+    				
+    			}
+    		}
+    	}
+    	Arrays.sort(T);
+    	int res = T[nums.length-1];
+    	return res;
+    }
+}
