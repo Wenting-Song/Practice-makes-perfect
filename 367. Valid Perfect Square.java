da0576e92849e5c3464254d75798dc69c0614490
Wenting Song <song1992katherine@hotmail.com>
2017-05-13 17:49:03 -0400

    Create 367. Valid Perfect Square.java

---
+++ 367. Valid Perfect Square.java
@@ -0,0 +1,21 @@
+public class Solution {
+	  public boolean isPerfectSquare(int num) {
+		long low = 1;
+		long high = num/2;
+		long nums = (long)num;
+		if(num == 1) return true;
+		while (low <=high) {
+			long mid = low + (high - low) / 2;
+			if(mid*mid == num){
+				return true;
+			}else if(mid*mid < num) {
+				low = mid + 1;
+
+			} else {
+				high = mid-1 ;
+			}
+
+		}
+		return false;
+	}
+}
